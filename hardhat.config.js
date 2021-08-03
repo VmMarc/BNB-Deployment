501b9e2d5886bfc286df1e0190b6e079d4759d61
bilaz666 <victor.me.marc@gmail.com>
2021-08-03 19:11:33 +0200

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,35 +2,19 @@ require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-solhint');
 require('hardhat-docgen');
 
-require('dotenv').config();
-const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
-
+const secret = require('./secret');
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: '0.8.5',
   networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    bscTestnet: {
+      url: secret.url,
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [secret.key],
+      // testnet link: https://testnet.bscscan.com/
     },
   },
   docgen: {
